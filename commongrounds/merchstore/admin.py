33428453b94cd12b448d05f5ce020340512b36a7
lioraongsamson-sketch <liora.ongsamson@student.ateneo.edu>
2026-03-05 22:34:15 +0800

    Added the admin panel

--- commongrounds/merchstore/admin.py
+++ commongrounds/merchstore/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import Product, ProductType
+
+
+class ProductIngredientInLine(admin.TabularInline):
+    model = Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    model = ProductType
+    inlines = [ProductIngredientInLine,]
+
+
+admin.site.register(ProductType, ProductAdmin)
 # Register your models here.
