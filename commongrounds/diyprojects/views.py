3c4b6768e7b95b39c84c74af543a42e84c1f312e
immabutter <yurich.gabriel.duro@student.ateneo.edu>
2026-03-06 01:09:01 +0800

    Created urls.py

--- commongrounds/diyprojects/views.py
+++ commongrounds/diyprojects/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from .models import Project
 
-# Create your views here.
+
+class ProjectListView(ListView):
+    model = Project
+    template_name = "project_list.html"
+
+
+class ProjectDetailView(DetailView):
+    model = Project
+    template_name = "project_detail.html"
