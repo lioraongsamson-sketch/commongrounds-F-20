b03cb3f08c051c3c63ad3e8ace03b58cd3e417b4
immabutter <yurich.gabriel.duro@student.ateneo.edu>
2026-03-06 01:12:13 +0800

    Created views, models and templates

--- commongrounds/diyprojects/admin.py
+++ commongrounds/diyprojects/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Project, ProjectCategory
+
+
+class ProjectCategoryInLine(admin.TabularInline):
+    model = Project
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    model = ProjectCategory
+    inlines = [ProjectCategoryInLine,]
+
+
+admin.site.register(ProjectCategory, ProjectAdmin)
--- commongrounds/diyprojects/views.py
+++ commongrounds/diyprojects/views.py
@@ -5,9 +5,11 @@ from .models import Project
 
 class ProjectListView(ListView):
     model = Project
+    context_object_name = 'projects'
     template_name = "project_list.html"
 
 
 class ProjectDetailView(DetailView):
     model = Project
+    context_object_name = 'project'
     template_name = "project_detail.html"
