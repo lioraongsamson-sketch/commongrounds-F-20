8e9b76e2a0f8f872685ca316e3bd67e129a4da55
lioraongsamson-sketch <liora.ongsamson@student.ateneo.edu>
2026-03-05 22:26:20 +0800

    Added paths to both urls.py files

---
+++ commongrounds/merchstore/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .views import ProductDetailView, ProductListView
+
+app_name = 'merchstore'
+
+urlpatterns = [
+    path('merchstore/items', ProductListView.as_view(), name="product_list"),
+    path('merchstore/item/<int:pk>',
+         ProductDetailView.as_view(), name="product_detail")
+]
