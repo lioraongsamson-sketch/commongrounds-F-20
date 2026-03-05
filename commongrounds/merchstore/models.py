e84ee01798abc7761ca8f8ab1187e1c982fffdf4
lioraongsamson-sketch <liora.ongsamson@student.ateneo.edu>
2026-03-05 22:22:08 +0800

    Created ProductType Model

--- commongrounds/merchstore/models.py
+++ commongrounds/merchstore/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.urls import reverse
+
+
+class ProductType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        ordering = ['name']
 
 # Create your models here.
