ed778062049abf8204b9754b3b63e40418f751ca
Phoebe <phoebebangco@gmail.com>
2026-03-06 14:35:08 +0800

    Created models and app-level urls.py

--- commongrounds/commissions/models.py
+++ commongrounds/commissions/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class CommissionType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Commission(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    people_required = models.PositiveIntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('commissions:request_detail', args=[str(self.id)])
+
+    class Meta:
+        ordering = ["created_on"]
