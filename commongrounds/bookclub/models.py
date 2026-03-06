73ba6985fd6d343729ae37315d81ba04138109f4
felizemendoza <felize.maria.mendoza@student.ateneo.edu>
2026-03-06 11:48:23 +0800

    Created models and views

--- commongrounds/bookclub/models.py
+++ commongrounds/bookclub/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.urls import reverse
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = "Genre"
+        verbose_name_plural = "Genres"
+        ordering = ['name']
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    genre = models.ForeignKey(Genre, on_delete=models.SET_NULL, null=True, related_name='books')
+    author = models.CharField()
+    publication_year = models.IntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title}"
+
+    def get_absolute_url(self):
+        return reverse("bookclub:book_detail", args=[str(self.pk)])
+
+    class Meta:
+        verbose_name = "Book"
+        verbose_name_plural = "Books"
+        ordering = ['-publication_year']
+
 
 # Create your models here.
--- commongrounds/bookclub/views.py
+++ commongrounds/bookclub/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Book
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+
+class BookListView(ListView):
+    model = Book
+
+
+class BookDetailView(DetailView):
+    model = Book
 
 # Create your views here.
