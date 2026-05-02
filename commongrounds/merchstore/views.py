f6b9b4681e3b386615f8cf4efde75642b371ad5c
lioraongsamson-sketch <liora.ongsamson@student.ateneo.edu>
2026-05-02 10:19:41 +0800

    Updated ProductListView in Views

--- commongrounds/merchstore/views.py
+++ commongrounds/merchstore/views.py
@@ -7,6 +7,22 @@ class ProductListView(ListView):
     model = Product
     template_name = "product_list.html"
 
+    def get_queryset(self):
+        queryset = Product.objects.all()
+
+        if self.request.user.is_authenticated:
+            queryset = queryset.exclude(seller=self.request.user)
+        return queryset
+    
+    def get_context_object_name(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        if self.reqest.user.is_authenticated:
+            context['products_user'] = Product.object.filter(seller=self.request.user)
+        else:
+            context['product_user'] = None
+        return context
+
 
 class ProductDetailView(DetailView):
     model = Product
