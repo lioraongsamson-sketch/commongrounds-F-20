8b2e7f7a15dd310352cc873b771e5d366d074e5c
felizemendoza <felize.maria.mendoza@student.ateneo.edu>
2026-05-03 16:10:39 +0800

    Added forms.py, updated BookDetailView and book_detail.html

---
+++ commongrounds/bookclub/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Book, BookReview
+
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = "__all__"
+
+
+class BookReviewForm(forms.ModelForm):
+    class Meta:
+        model = BookReview
+        fields = ["title", "comment"]
--- commongrounds/bookclub/views.py
+++ commongrounds/bookclub/views.py
@@ -1,6 +1,10 @@
-from .models import Book
+from .models import Book, BookReview
+from .forms import BookForm, BookReviewForm
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
 
 
 class BookListView(ListView):
@@ -8,6 +12,25 @@ class BookListView(ListView):
     template_name = "book_list.html"
 
 
+
 class BookDetailView(DetailView):
     model = Book
     template_name = "book_detail.html"
+    reviews = BookReview.objects.all()
+    reviewgroups = Book.objects.all()
+    ctx = {
+        "bookreview_list": reviews, "reviewgroup": reviewgroups
+    }
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["book"] = Book.objects.all()
+        context["review"] = BookReviewForm()
+        return context
+    
+    def post(self, request, *args, **kwargs):
+        review = BookReviewForm(request.POST)
+        if review.is_valid():
+            review.instance.book_id = self.kwargs["pk"]
+            review.save()
+        return self.get(request, *args, **kwargs)
