aed04f8feb01aa4c6dab4fe187bf6b2d7fc543a1
lioraongsamson-sketch <liora.ongsamson@student.ateneo.edu>
2026-05-03 14:44:23 +0800

    Updated merchstore app's urls

--- commongrounds/merchstore/urls.py
+++ commongrounds/merchstore/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import ProductDetailView, ProductListView
+from .views import ProductDetailView, ProductListView, ProductCreateView
+from .views import ProductUpdateView, CartView, TransactionListView
 
 app_name = 'merchstore'
 
-urlpatterns = [
+urlpatterns =[
     path('items', ProductListView.as_view(), name="product_list"),
+    path('item/add', ProductCreateView.as_view(), name="product_create"),
     path('item/<int:pk>',
-         ProductDetailView.as_view(), name="product_detail")
+         ProductDetailView.as_view(), name="product_detail"),
+    path('item/<int:pk>/edit', ProductUpdateView.as_view(), name="product_update"),
+    path('cart', CartView.as_view(), name="cart_view"),
+    path('transactions', TransactionListView.as_view(), name="transaction_list")
 ]
