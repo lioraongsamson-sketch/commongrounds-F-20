5e1fc149e67ed17b0426656673beb192c84e20e4
Phoebe <phoebebangco@gmail.com>
2026-05-04 21:03:54 +0800

    Changed Commission model and added new Job and JobApplication models

--- commongrounds/commissions/models.py
+++ commongrounds/commissions/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from accounts.models import Profile
 
 
 class CommissionType(models.Model):
@@ -14,9 +15,24 @@ class CommissionType(models.Model):
 
 
 class Commission(models.Model):
+    STATUSES = [('O', 'Open'), ('F', 'Full')]
+
     title = models.CharField(max_length=255)
     description = models.TextField()
+    type = models.ForeignKey(
+        CommissionType,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='commission'
+    )
+    maker = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        null=True,
+        related_name='commission'
+    )
     people_required = models.PositiveIntegerField()
+    status = models.CharField(choices=STATUSES, default=STATUSES[0])
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
@@ -28,3 +44,38 @@ class Commission(models.Model):
 
     class Meta:
         ordering = ["created_on"]
+
+class Job(models.Model):
+    STATUSES = [('O', 'Open'), ('F', 'Full')]
+    commission = models.ForeignKey(
+        Commission,
+        on_delete=models.CASCADE,
+        null=True,
+        related_name='job'
+    )
+    role = models.CharField(max_length=255)
+    manpower_required = models.PositiveIntegerField()
+    status = models.CharField(choices=STATUSES, default=STATUSES[0])
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["status", "-manpower_required", "role"]
+
+class JobApplication(models.Model):
+    STATUSES = [('P', 'Pending'), ('A', 'Accepted'), ('R', 'Rejected')]
+    job = models.ForeignKey(
+        Job,
+        on_delete=models.CASCADE,
+        null=True,
+        related_name='job_application'
+    )
+    status = models.CharField(choices=STATUSES, default=STATUSES[0])
+    applied_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["status", "-applied_on"]
