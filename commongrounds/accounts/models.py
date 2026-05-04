398e8d80c9a830c1a87029e34539fdd40db3a203
lioraongsamson-sketch <liora.ongsamson@student.ateneo.edu>
2026-05-04 20:28:09 +0800

    Updated models.py based on accounts app

--- commongrounds/accounts/models.py
+++ commongrounds/accounts/models.py
@@ -1,15 +1,19 @@
-from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 
+USER_ROLES ={
+    "Market Seller": "Market Seller",
+    "Event Organizer": "Event Organizer",
+    "Book Contributor": "Book Contributor",
+    "Project Creator": "Project Creator",
+    "Commission Maker": "Commission Maker",
+}
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     display_name = models.CharField(max_length=63)
     email = models.EmailField()
+    role = models.CharField(max_length=50, choices=USER_ROLES, blank=True, null=True)
 
     def __str__(self):
         return self.display_name
-
-    def get_absolute_url(self):
-        return reverse('accounts:profile_update', args=[str(self.user.username)])
