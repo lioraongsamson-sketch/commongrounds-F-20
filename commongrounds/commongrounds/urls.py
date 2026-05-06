7bf72694313f9aacc6d7eefbc581006d213ddfdb
immabutter <yurich.gabriel.duro@student.ateneo.edu>
2026-05-06 21:35:52 +0800

    Added homepage implementation

--- commongrounds/commongrounds/urls.py
+++ commongrounds/commongrounds/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from .views import HomePageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomePageView.as_view(), name='homepage'),
     path('localevents/', include('localevents.urls', namespace='localevents')),
     path('bookclub/', include('bookclub.urls', namespace='bookclub')),
     path('merchstore/', include('merchstore.urls', namespace="merchstore")),
