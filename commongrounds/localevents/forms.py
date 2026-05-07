fa3ca1b062970f8119db36ee66d2cebd38a71cb1
Elite-TS <venicci.mikhail.palmiano@student.ateneo.edu>
2026-05-07 17:44:52 +0800

    Revert "Implemented Create View and Started Update View"
    This reverts commit 05e5b9674ce88b4aab8f7bbd469417956a7679b2.

--- commongrounds/localevents/forms.py
+++ commongrounds/localevents/forms.py
@@ -6,20 +6,4 @@ class EventForm(forms.ModelForm):
     class Meta:
         model = Event
         fields = ['title', 'category', 'event_image', 'description',
-                  'location', 'start_time', 'end_time', 'event_capacity', 'event_signups', 'status']
-        widgets = {
-            'start_time': forms.TextInput(
-                attrs={'type': 'datetime-local'}
-            ),
-            'end_time': forms.TextInput(
-                attrs={'type': 'datetime-local'}
-            )
-        }
-
-
-class EventUpdateForm(forms.ModelForm):
-    class Meta:
-        model = Event
-        # exclude = ('organizer',)
-        fields = ['title', 'category', 'event_image', 'description',
-                  'location', 'start_time', 'end_time', 'event_capacity', 'event_signups', 'status']
+                  'location', 'start_time', 'end_time', 'event_capacity', 'status']
--- commongrounds/localevents/urls.py
+++ commongrounds/localevents/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from .views import EventListView, EventDetailView, EventSignup, EventCreateView, EventUpdateView
+from .views import EventListView, EventDetailView, EventSignup, EventCreateView
 
 app_name = 'localevents'
 
 urlpatterns = [
     path('events', EventListView.as_view(), name='event_list'),
     path('event/add', EventCreateView.as_view(), name="event_create"),
-    path('item/<int:pk>/edit', EventUpdateView.as_view(), name="event_update"),
     path(
         'event/<int:pk>/',
         EventDetailView.as_view(),
